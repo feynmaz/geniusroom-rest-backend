e0b0bbe453a3543c0318b7e0d127d74ae77f6847
feynmaz <feynmaz@gmail.com>
2021-06-28 22:39:49 +0500

    добавил контент

--- geniusroom_rest_backend/settings.py
+++ geniusroom_rest_backend/settings.py
@@ -144,8 +144,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'server/media/')
-MEDIA_URL = 'server/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
